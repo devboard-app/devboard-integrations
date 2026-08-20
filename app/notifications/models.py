b9415137f1eeed59282aab62a5dab7b698c50be9
Constantin Ilie <intern.constantin.ilie@assist.ro>
2026-08-20 11:18:44 +0300

    feat: added models + migrations

---
+++ app/notifications/models.py
@@ -0,0 +1,18 @@
+import uuid
+from datetime import datetime, timezone
+
+from app.db import db
+
+
+class Notification(db.Model):
+    __tablename__ = "notifications"
+
+    id = db.Column(db.Uuid, primary_key=True, default=uuid.uuid4)
+    recipient_id = db.Column(db.Uuid, nullable=False)
+    type = db.Column(db.String, nullable=False)
+    message = db.Column(db.Text, nullable=False)
+    read = db.Column(db.Boolean, nullable=False, default=False)
+    link = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc))
+
+    
